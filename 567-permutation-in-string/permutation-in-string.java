18ab42dd371399f09004c533b6a9b36ef904acb1
neha25-08 <neha4jindal@gmail.com>
2025-04-11 19:53:21 +0530

    Time: 8 ms (61.05%) | Memory: 42.8 MB (60.00%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,30 +1,33 @@
 class Solution {
-    public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) return false;
-
-        int[] count = new int[26]; // For a-z
-
-        for (int i = 0; i < s1.length(); i++) {
-            count[s1.charAt(i) - 'a']++;
-            count[s2.charAt(i) - 'a']--;
-        }
-
-        if (allZero(count)) return true;
-
-        for (int i = s1.length(); i < s2.length(); i++) {
-            count[s2.charAt(i) - 'a']--;
-            count[s2.charAt(i - s1.length()) - 'a']++;
-
-            if (allZero(count)) return true;
-        }
-
+    boolean allZero(int[] a)
+    {
+        for(int i: a)
+        if(i!=0) 
         return false;
-    }
-
-    private boolean allZero(int[] count) {
-        for (int c : count) {
-            if (c != 0) return false;
-        }
         return true;
+    }
+    public boolean checkInclusion(String s1, String s2) {
+        int n=s2.length();
+        if(s2.length()<s1.length())
+        {
+            return false;
+        }
+        //s2+=s2;
+        int[] arr=new int[26];
+        for(int i=0;i<s1.length();i++)
+        {
+            arr[s1.charAt(i)-'a']--;
+            arr[s2.charAt(i)-'a']++;
+        }
+        if (allZero(arr)) return true;
+        //return true;
+        for(int i=1;i<=n-s1.length();i++)
+        {
+             arr[s2.charAt(i-1)-'a']--;
+             arr[s2.charAt(i+s1.length()-1)-'a']++;
+            if (allZero(arr))
+            return true;
+        }
+        return false;
     }
 }
